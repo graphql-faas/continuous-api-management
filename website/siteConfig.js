7f90f220d9822efd7383fb84147d35ce7f2bcf11
dalongrong <1141591465@qq.com>
2018-11-20 08:43:53 +0800

    change build

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,10 +21,10 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
+  title: '文档博客', // Title for your website.
+  tagline: '文档博客',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: 'https://graphql-faas.github.io', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
